f74c976352bd3494ccb1a642ebbc1e6fdb0c687e
amulyabharatha <amulya.bharatha@infosys.com>
2023-09-11 15:11:22 +0530

    added roles

--- my_config.sh
+++ my_config.sh
@@ -4,3 +4,4 @@ export PREFIX= #the prefix you want for you S3 bucket e.g., DEMO-scikit-iris-gus
 export FRAMEWORK_VERSION=1.0-1
 export SCRIPT_PATH=train.py
 export INSTANCE_TYPE= #the instance size you want to use, i.e., ml.m5.large
+#added roles
