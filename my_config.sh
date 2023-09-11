94e16608ffc6b96274ed5857c4e7206d0dff07a3
amulyabharatha <amulya.bharatha@infosys.com>
2023-09-11 15:09:29 +0530

    added files

--- my_config.sh
+++ my_config.sh
@@ -1,4 +1,4 @@
-export SG_ROLE= #An AWS IAM Role with Full SG Access (I give full S3 Access too fwiw )
+export SG_ROLE= arn:aws:iam::657605447075:role/service-role/AmazonSageMaker-ExecutionRole-20230911T145444 #An AWS IAM Role with Full SG Access (I give full S3 Access too fwiw )
 export WORK_DIRECTORY= #the name of your local data_dir, i.e., data
 export PREFIX= #the prefix you want for you S3 bucket e.g., DEMO-scikit-iris-gus
 export FRAMEWORK_VERSION=1.0-1
