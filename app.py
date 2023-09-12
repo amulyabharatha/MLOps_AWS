0351e0afdf987d3d6b79f9b21c538848390b4944
amulyabharatha <amulya.bharatha@infosys.com>
2023-09-12 12:13:22 +0530

    added model

--- app.py
+++ app.py
@@ -131,5 +131,5 @@ if __name__ == "__main__":
     #model_data = train_model(session_details, train_input)
     test_data = gen_test_data()
     #serve_model(model_data, test_data)
-    model_data = {"model_path": "s3://sagemaker-us-east-1-316381839854/sagemaker-scikit-learn-2023-01-08-18-35-09-467/output/model.tar.gz"}
+    model_data = {"model_path": "s3://sagemaker-us-east-1-657605447075/sagemaker-scikit-learn-2023-09-12-06-33-48-706/source/sourcedir.tar.gz"}
     serve_saved_model(model_data, test_data)
